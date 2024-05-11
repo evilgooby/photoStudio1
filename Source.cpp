fb124617c5a9f78053d58ea879bbb4322fa382f2
evilgooby <evilgooby1@gmail.com>
2024-05-11 16:13:54 +0300

    Добавил виртуальные функции

--- Source.cpp
+++ Source.cpp
@@ -9,46 +9,59 @@ protected:
     string name;
     int age;
     string gender;
-
 public:
     Person(string name, int age, string gender) : name(name), age(age), gender(gender) {}
+
+    virtual void consolOut(){
+        std::cout<<"Имя: "<<name<<std::endl<<"Возраст:"<<age<<std::endl<<"Пол:" <<gender<<std::endl;
+    }
 
     string getName() const {
         return name;
     }
-
     int getAge() const {
         return age;
     }
-
     string getGender() const {
         return gender;
     }
 };
 
+
 class Photographer : public Person {
 private:
     string specialty;
-
 public:
     Photographer(string name, int age, string gender, string specialty) : Person(name, age, gender), specialty(specialty) {}
-
+    void consolOut() override{
+        std::cout<<"Имя: "<<name<<std::endl<<
+        "Возраст:"<<age<<std::endl<<
+        "Пол:" <<gender<<std::endl<<
+        "Специализация: " << specialty<<std::endl;
+    }
     void takePhoto() const {
         cout << name << " is taking a photo." << endl;
     }
 };
 
+
 class Client : public Person {
 private:
     string email;
-
 public:
     Client(string name, int age, string gender, string email) : Person(name, age, gender), email(email) {}
+    void consolOut() override{
+        std::cout<<"Имя: "<<name<<std::endl
+        <<"Возраст:"<<age<<std::endl
+        <<"Пол:"<<gender<<std::endl
+        <<"Почта: "<<email<<std::endl;
 
+    }
     string getEmail() const {
         return email;
     }
 };
+
 
 class PhotoStudio {
 private:
@@ -71,6 +84,8 @@ public:
     }
 };
 
+
+
 int main() {
     // Creating objects
     Photographer john_doe("John Doe", 30, "Male", "Portrait");
@@ -79,10 +94,8 @@ int main() {
 
     // We hire a photographer
     studio.hirePhotographer(john_doe);
-
     // Registering a client
     studio.registerClient(jane_smith);
-
     // take photo
     john_doe.takePhoto();
 
